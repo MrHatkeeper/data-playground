0181a7a9babd433ffac5636add0ac70b324bcafc
MrHatkeeper <karel.krch@educanet.cz>
2021-12-10 08:32:31 +0100

    .asSequence()

--- src/main/kotlin/processing/data_processing.kt
+++ src/main/kotlin/processing/data_processing.kt
@@ -27,16 +27,16 @@ fun countMaleStudents(students: List<Student>): Int {
 // gender == Gender.FEMALE
 // or gender.name == "FEMALE"
 fun avgAgeOfFemaleStudent(students: List<Student>): Double {
-    return students.filter { it.gender == Gender.FEMALE }.map { it.age }.average()
+    return students.asSequence().filter { it.gender == Gender.FEMALE }.map { it.age }.average()
 }
 
 fun getProductOfStudentAges(students: List<Student>): Int {
-    return students.map { it.age }.reduce { it1, it2 -> it1 + it2}
+    return students.asSequence().map { it.age }.reduce { it1, it2 -> it1 + it2}
 }
 
 // ignore F Grades
 fun productOfStudentGrades(student: Student): Int {
-    return student.grades.filter { it.type != GradeType.F}.map{it.type.value}.reduce {it1, it2 -> it1 * it2}
+    return student.grades.asSequence().filter { it.type != GradeType.F}.map{it.type.value}.reduce {it1, it2 -> it1 * it2}
 }
 
 // region BONUS
